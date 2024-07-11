f04e410bc6e56b3bc99988d450df8062e5eb9818
Nicolas Alex <nicolasmunozj0@gmail.com>
2024-07-10 20:47:23 -0500

    Avanzamos variables

---
+++ variables/variables.py
@@ -0,0 +1,32 @@
+"""
+
+num = 10 
+num +=5
+num -=5
+print(num)
+"""
+
+""" 
+pais = "Colombia"
+gana = True
+mensaje = " Hoy gana "+ pais  + "?  "+ "\n" + str(gana)
+print(mensaje)
+"""
+
+""" nombre = 5
+bienvenida = f"hola   {nombre}    feliz noche"
+del bienvenida
+print (bienvenida) """
+
+nombre = True
+bienvenido = f"hola {nombre} feliz noche"
+print (bienvenido)
+print ("ola" in bienvenido)
+print("nicolas" not in bienvenido)
+print("feliz" not in bienvenido)
+
+#camel case
+nombreCompleto="Alexander Muñoz"
+
+#snakeCase
+nombre_completo="Alexander Muñoz"
