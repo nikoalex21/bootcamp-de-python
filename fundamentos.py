832a3d5789023820299626a1214d6b0c60eca6a4
Nicolas Alex <nicolasmunozj0@gmail.com>
2024-07-15 19:50:08 -0500

    se abordaron fundamentos de python

---
+++ fundamentos.py
@@ -0,0 +1,10 @@
+name ="       Alexander       "
+print(type(name))
+print(name[0:3])
+last_name = "Alexander Muñoz "
+print(name*5)
+print(len(name))
+print(name.lower())#pasar todo a minuscula
+print(name.upper())#pasar todo a mayuscula
+print(name.strip())#elimina espacios
+print(name + " " + last_name)#concatenar str
